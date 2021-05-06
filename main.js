b52fd4c508d8582c98f2f77a320f7d443a0bea9e
ericksonder <ericksonder017@gmail.com>
2021-05-06 03:23:09 -0500

    Funcionalidadcompleta

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondDegrees = (seconds / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`
+    
+    
+    const minutes = now.getMinutes();
+    const minutDegrees = (minutes /  60) * 360 + 90;
+    minHand.style.transform = `rotate(${minutDegrees}deg)`
+
+    const hour = now.getHours();
+    const hourDegrees = (hour / 12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+    
+}
+
+
+
+
+setInterval(setDate, 1000);
